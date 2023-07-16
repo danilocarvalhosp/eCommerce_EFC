5e583d4b53c2afcb342571c64e75e694d815c87e
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-07-16 19:35:02 -0300

    EF Seeds - Carga inicial

--- eCommerce/eCommerce.API/Database/eCommerceContext.cs
+++ eCommerce/eCommerce.API/Database/eCommerceContext.cs
@@ -22,5 +22,18 @@ namespace eCommerce.API.Database
         public DbSet<Contato> Contatos { get; set; }
         public DbSet<EnderecoEntrega> EnderecosEntrega { get; set; }
         public DbSet<Departamento> Departamentos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Departamento>().HasData(
+                new Departamento { Id = 1, Nome = "Moda" },
+                new Departamento { Id = 2, Nome = "Informática" },
+                new Departamento { Id = 3, Nome = "Eletrodomésticos" },
+                new Departamento { Id = 4, Nome = "Eletroportáteis" },
+                new Departamento { Id = 5, Nome = "Beleza" },
+                new Departamento { Id = 6, Nome = "Mercado" },
+                new Departamento { Id = 7, Nome = "Móveis" }
+            );
+        }
     }
 }
