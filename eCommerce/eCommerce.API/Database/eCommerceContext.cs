9eef2b3427ac89dc3149c31e49de304771e54001
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-07-16 17:17:42 -0300

    realizando a conexão pelo Construtor

--- eCommerce/eCommerce.API/Database/eCommerceContext.cs
+++ eCommerce/eCommerce.API/Database/eCommerceContext.cs
@@ -1,6 +1,23 @@
-﻿namespace eCommerce.API.Database
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.API.Database
 {
-    public class eCommerceContext
+    public class eCommerceContext : DbContext
     {
+        #region Conexão sem distinção de ambientes de execução
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;");
+        //}
+        #endregion
+
+        #region Conexão com distinção de ambientes de execução
+
+        public eCommerceContext(DbContextOptions<eCommerceContext> options) : base(options)
+        {
+            
+        }
+
+        #endregion
     }
 }
--- eCommerce/eCommerce.API/Program.cs
+++ eCommerce/eCommerce.API/Program.cs
@@ -1,4 +1,6 @@
+using eCommerce.API.Database;
 using eCommerce.API.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<eCommerceContext>(
+    option => option.UseSqlServer(builder.Configuration.GetConnectionString("eCommerce")));
+
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 #endregion
 
