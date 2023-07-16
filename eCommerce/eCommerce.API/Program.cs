eea00b883d86b9836a09534184278615f7f34753
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-07-16 10:49:15 -0300

    Configurando a Injeção de Dependência (DI)

--- eCommerce/eCommerce.API/Program.cs
+++ eCommerce/eCommerce.API/Program.cs
@@ -1,14 +1,20 @@
+using eCommerce.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
+#region ConfigureService()
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<UsuarioRepository>();
+#endregion
 
 var app = builder.Build();
 
+#region Configure()
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -23,3 +29,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+#endregion
