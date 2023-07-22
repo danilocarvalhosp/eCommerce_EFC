f932a2ea17128a608c52281ce6305faea0381833
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-07-22 13:44:10 -0300

    Criando um novo projeto

---
+++ eCommerce/eCommerce.Console.Query/eCommerceContext.cs
@@ -0,0 +1,30 @@
+﻿using eCommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.API.Database
+{
+    public class eCommerceContext : DbContext
+    {
+        #region Conexão sem distinção de ambientes de execução
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;");
+                //.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
+                //.EnableSensitiveDataLogging();
+        }
+        #endregion
+
+        #region Conexão com distinção de ambientes de execução
+        //public eCommerceContext(DbContextOptions<eCommerceContext> options) : base(options)
+        //{
+            
+        //}
+        #endregion
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Contato> Contatos { get; set; }
+        public DbSet<EnderecoEntrega> EnderecosEntrega { get; set; }
+        public DbSet<Departamento> Departamentos { get; set; }
+    }
+}
