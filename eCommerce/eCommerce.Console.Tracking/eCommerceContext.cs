b07556c259895236da74af553343e1f72f677b4f
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-07-22 09:37:56 -0300

    Habilitando Log

--- eCommerce/eCommerce.Console.Tracking/eCommerceContext.cs
+++ eCommerce/eCommerce.Console.Tracking/eCommerceContext.cs
@@ -6,10 +6,13 @@ namespace eCommerce.API.Database
     public class eCommerceContext : DbContext
     {
         #region Conexão sem distinção de ambientes de execução
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;");
-        //}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;")
+                .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
         #endregion
 
         #region Conexão com distinção de ambientes de execução
@@ -23,18 +26,5 @@ namespace eCommerce.API.Database
         public DbSet<Contato> Contatos { get; set; }
         public DbSet<EnderecoEntrega> EnderecosEntrega { get; set; }
         public DbSet<Departamento> Departamentos { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Departamento>().HasData(
-                new Departamento { Id = 1, Nome = "Moda" },
-                new Departamento { Id = 2, Nome = "Informática" },
-                new Departamento { Id = 3, Nome = "Eletrodomésticos" },
-                new Departamento { Id = 4, Nome = "Eletroportáteis" },
-                new Departamento { Id = 5, Nome = "Beleza" },
-                new Departamento { Id = 6, Nome = "Mercado" },
-                new Departamento { Id = 7, Nome = "Móveis" }
-            );
-        }
     }
 }
