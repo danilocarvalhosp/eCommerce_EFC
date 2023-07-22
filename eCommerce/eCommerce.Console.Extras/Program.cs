8bcc6ef78b743f224a342d965a66b34ea7074668
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-07-22 18:28:22 -0300

    Tabelas Temporais - QueryTemporalAll e AsOf

--- eCommerce/eCommerce.Console.Extras/Program.cs
+++ eCommerce/eCommerce.Console.Extras/Program.cs
@@ -4,10 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 var db = new eCommerceContext();
 
-var usuarioNovo = db.Usuarios!.Find(2);
-usuarioNovo.NomeMae = "Joana Rodrigues de Almeida Santana";
-db.SaveChanges();
-
+#region Global Filter
 Console.WriteLine("========== GLOBAL FILTER (FILTROS GLOBAIS) ==========");
 
 //var usuarios = db.Usuarios!.IgnoreQueryFilters().ToList();
@@ -16,4 +13,18 @@ foreach (var usuario in usuarios)
 {
     Console.WriteLine($"{usuario.Nome}");
 }
+#endregion
 
+Console.WriteLine("========== DADOS TEMPORAIS (HISTÓRICO) ==========");
+
+/*
+// TEMPORALALL
+var usuarioTemp = db.Usuarios!.TemporalAll().Where(a => a.Id == 2).OrderByDescending(a => EF.Property<DateTime>(a, "PeriodoInicial")).ToList();
+*/
+
+var AsOf = new DateTime(2023, 07, 22, 21, 11, 00);
+var usuarioTemp = db.Usuarios!.TemporalAsOf(AsOf).Where(a => a.Id == 2).OrderByDescending(a => EF.Property<DateTime>(a, "PeriodoInicial")).ToList();
+foreach (var usuario in usuarioTemp)
+{
+    Console.WriteLine($" - {usuario.Nome} - Mãe: {usuario.NomeMae}");
+}
