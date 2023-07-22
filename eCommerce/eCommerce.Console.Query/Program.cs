03f215ff14c7250a532976a04a03e17583eea10a
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-07-22 14:38:19 -0300

    Single, Count, Max e Min

--- eCommerce/eCommerce.Console.Query/Program.cs
+++ eCommerce/eCommerce.Console.Query/Program.cs
@@ -1,4 +1,5 @@
 ﻿using eCommerce.API.Database;
+using eCommerce.Models;
 
 var db = new eCommerceContext();
 
@@ -10,8 +11,9 @@ foreach (var usuario in usuarios)
     Console.WriteLine($" - {usuario.Nome}");
 }
 
-// FIND
 Console.WriteLine("BUSCAR UM ÚNICO USUÁRIO");
+/*
+// FIND
 var usuario01 = db.Usuarios!.Find(2);
 Console.WriteLine($" CÓDIGO: {usuario01!.Id} - NOME: {usuario01!.Nome}");
 
@@ -19,7 +21,7 @@ Console.WriteLine($" CÓDIGO: {usuario01!.Id} - NOME: {usuario01!.Nome}");
 var usuario02 = db.Usuarios!.Where(a => a.Email == "cassia@gmail.com").First();
 Console.WriteLine($" CÓDIGO: {usuario02!.Id} - NOME: {usuario02!.Nome}");
 
-//FIRSTORDEFAULT
+// FIRSTORDEFAULT
 var usuario05 = db.Usuarios!.Where(a => a.Email == "cassia@gmail.com").FirstOrDefault();
 if (usuario05 == null)
 {
@@ -34,7 +36,7 @@ else
 var usuario03 = db.Usuarios!.OrderBy(a => a.Id).Last();
 Console.WriteLine($" CÓDIGO: {usuario03!.Id} - NOME: {usuario03!.Nome}");
 
-//LASTORDEFAULT
+// LASTORDEFAULT
 var usuario04 = db.Usuarios!.Where(a => a.Email == "cassia@gmail.com").LastOrDefault();
 if (usuario04 == null)
 {
@@ -44,3 +46,31 @@ else
 {
     Console.WriteLine($" CÓDIGO: {usuario04!.Id} - NOME: {usuario04!.Nome}");
 }
+*/
+
+// SINGLE
+var usuario01 = db.Usuarios!.Single(a => a.Email == "danilo@gmail.com");
+Console.WriteLine($" CÓDIGO: {usuario01!.Id} - NOME: {usuario01!.Nome}");
+
+// SINGLE
+var usuario02 = db.Usuarios!.SingleOrDefault(a => a.Email == "danilosp@gmail.com");
+if (usuario02 == null)
+{
+    Console.WriteLine("Usuário não encontrado!!!");
+}
+else
+{
+    Console.WriteLine($" CÓDIGO: {usuario02!.Id} - NOME: {usuario02!.Nome}");
+}
+
+// COUNT
+var count = db.Usuarios!.Where(a => a.Nome.Contains("o")).Count();
+Console.WriteLine($"NÚMERO DE USUÁRIOS QUE CONTÉM A LETRA 'O' EM SEU NOME: {count}");
+
+// MAX
+var max = db.Usuarios!.Max(a => a.Nome);
+Console.WriteLine($"VALOR MÁXIMO: {max}");
+
+// MIN
+var min = db.Usuarios!.Min(a => a.Nome);
+Console.WriteLine($"VALOR MÍNIMO: {min}");
