4148de2231429eed24533c18cae07a08308c6482
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-07-22 18:37:11 -0300

    Tabelas Temporais - ContainedIn, Between e FromTo

--- eCommerce/eCommerce.Console.Extras/Program.cs
+++ eCommerce/eCommerce.Console.Extras/Program.cs
@@ -20,11 +20,19 @@ Console.WriteLine("========== DADOS TEMPORAIS (HISTÓRICO) ==========");
 /*
 // TEMPORALALL
 var usuarioTemp = db.Usuarios!.TemporalAll().Where(a => a.Id == 2).OrderByDescending(a => EF.Property<DateTime>(a, "PeriodoInicial")).ToList();
-*/
 
+// TEMPORALASOF
 var AsOf = new DateTime(2023, 07, 22, 21, 11, 00);
 var usuarioTemp = db.Usuarios!.TemporalAsOf(AsOf).Where(a => a.Id == 2).OrderByDescending(a => EF.Property<DateTime>(a, "PeriodoInicial")).ToList();
-foreach (var usuario in usuarioTemp)
+*/
+
+var From = new DateTime(2023, 07, 22, 21, 09, 00);
+var To = new DateTime(2023, 07, 22, 21, 13, 00);
+var usuarioTemp = db.Usuarios!.TemporalFromTo(From, To).Where(a => a.Id == 2).OrderByDescending(a => EF.Property<DateTime>(a, "PeriodoInicial")).ToList();
+var usuarioTemp2 = db.Usuarios!.TemporalBetween(From, To).Where(a => a.Id == 2).OrderByDescending(a => EF.Property<DateTime>(a, "PeriodoInicial")).ToList();
+var usuarioTemp3 = db.Usuarios!.TemporalContainedIn(From, To).Where(a => a.Id == 2).OrderByDescending(a => EF.Property<DateTime>(a, "PeriodoInicial")).ToList();
+
+foreach (var usuario in usuarioTemp3)
 {
     Console.WriteLine($" - {usuario.Nome} - Mãe: {usuario.NomeMae}");
 }
