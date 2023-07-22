8e4f077ab881d5058182d70034e825e2f52d21be
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-07-22 18:00:40 -0300

    Tabelas Temporais - Habilitação

--- eCommerce/eCommerce.Console.Extras/eCommerceContext.cs
+++ eCommerce/eCommerce.Console.Extras/eCommerceContext.cs
@@ -9,7 +9,7 @@ namespace eCommerce.API.Database
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder
-                .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;");
+                .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerceTemp;Integrated Security=True;");
         }
         #endregion
 
@@ -20,6 +20,15 @@ namespace eCommerce.API.Database
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Usuario>().ToTable("Usuarios", t => t.IsTemporal(
+                b =>
+                {
+                    b.HasPeriodStart("PeriodoInicial");
+                    b.HasPeriodEnd("PeriodoFinal");
+                    b.UseHistoryTable("UsuariosHistorico");
+                }
+                ));
+
             modelBuilder.Entity<Usuario>().HasQueryFilter(a => a.SituacaoCadastro == SituacaoCadastro.Ativo);
             modelBuilder.Entity<Usuario>().Property(a => a.SituacaoCadastro).HasConversion<string>();
         }
