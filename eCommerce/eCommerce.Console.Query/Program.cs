389f44fe2be9efcc3662ebe0c2e26740e3d80cc2
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-07-22 14:20:52 -0300

    Find, First, Last e suas variações

--- eCommerce/eCommerce.Console.Query/Program.cs
+++ eCommerce/eCommerce.Console.Query/Program.cs
@@ -9,3 +9,38 @@ foreach (var usuario in usuarios)
 {
     Console.WriteLine($" - {usuario.Nome}");
 }
+
+// FIND
+Console.WriteLine("BUSCAR UM ÚNICO USUÁRIO");
+var usuario01 = db.Usuarios!.Find(2);
+Console.WriteLine($" CÓDIGO: {usuario01!.Id} - NOME: {usuario01!.Nome}");
+
+// FIRST
+var usuario02 = db.Usuarios!.Where(a => a.Email == "cassia@gmail.com").First();
+Console.WriteLine($" CÓDIGO: {usuario02!.Id} - NOME: {usuario02!.Nome}");
+
+//FIRSTORDEFAULT
+var usuario05 = db.Usuarios!.Where(a => a.Email == "cassia@gmail.com").FirstOrDefault();
+if (usuario05 == null)
+{
+    Console.WriteLine("Usuário não encontrado!!!");
+}
+else
+{
+    Console.WriteLine($" CÓDIGO: {usuario02!.Id} - NOME: {usuario02!.Nome}");
+}
+
+// LAST
+var usuario03 = db.Usuarios!.OrderBy(a => a.Id).Last();
+Console.WriteLine($" CÓDIGO: {usuario03!.Id} - NOME: {usuario03!.Nome}");
+
+//LASTORDEFAULT
+var usuario04 = db.Usuarios!.Where(a => a.Email == "cassia@gmail.com").LastOrDefault();
+if (usuario04 == null)
+{
+    Console.WriteLine("Usuário não encontrado!!!");
+}
+else
+{
+    Console.WriteLine($" CÓDIGO: {usuario04!.Id} - NOME: {usuario04!.Nome}");
+}
