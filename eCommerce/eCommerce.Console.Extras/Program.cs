208176f8c6b482b0e23ebff601e39839dc0dfad7
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-07-22 18:14:07 -0300

    Tabelas Temporais - Inserindo dados no histórico

--- eCommerce/eCommerce.Console.Extras/Program.cs
+++ eCommerce/eCommerce.Console.Extras/Program.cs
@@ -1,9 +1,15 @@
 ﻿using eCommerce.API.Database;
+using eCommerce.Models;
 using Microsoft.EntityFrameworkCore;
+
+var db = new eCommerceContext();
+
+var usuarioNovo = db.Usuarios!.Find(2);
+usuarioNovo.NomeMae = "Joana Rodrigues de Almeida Santana";
+db.SaveChanges();
 
 Console.WriteLine("========== GLOBAL FILTER (FILTROS GLOBAIS) ==========");
 
-var db = new eCommerceContext();
 //var usuarios = db.Usuarios!.IgnoreQueryFilters().ToList();
 var usuarios = db.Usuarios!.ToList();
 foreach (var usuario in usuarios)
