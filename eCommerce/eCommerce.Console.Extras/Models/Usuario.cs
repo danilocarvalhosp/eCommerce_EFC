e7c994179856956d42dde6e82b238c007a6e27a2
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-07-22 17:23:21 -0300

    Criando projeto

---
+++ eCommerce/eCommerce.Console.Extras/Models/Usuario.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eCommerce.Console.Query.Config;
+
+
+namespace eCommerce.Models
+{
+    public class Usuario
+    {
+        private readonly Action<object, string> LazyLoader;
+
+        public Usuario()
+        {
+            
+        }
+
+        public Usuario(Action<object, string> lazyLoader)
+        {
+            LazyLoader = lazyLoader;
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? Sexo { get; set; }
+        public string RG { get; set; } 
+        public string CPF { get; set; } = null!;
+        public string? NomeMae { get; set; }
+        public string? NomePai { get; set; }
+        public string SituacaoCadastro { get; set; } = null!;
+        public DateTimeOffset DataCadastro { get; set; }
+        public Contato? Contato { get; set; }
+
+        private ICollection<EnderecoEntrega>? _enderecosEntrega;
+        public ICollection<EnderecoEntrega>? EnderecosEntrega {
+            get => LazyLoader.Load(this, ref _enderecosEntrega); 
+            set => _enderecosEntrega = value; 
+        }
+        public ICollection<Departamento>? Departamentos { get; set; }
+    }
+}
---
+++ eCommerce/eCommerce.Console.Extras/eCommerceContext.cs
@@ -0,0 +1,38 @@
+﻿using eCommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.API.Database
+{
+    public class eCommerceContext : DbContext
+    {
+        #region Conexão sem distinção de ambientes de execução
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                //.UseLazyLoadingProxies() - Habilita o Lazy Loading usando Proxies
+                .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;"
+                //, options => options.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)
+                )
+                .LogTo(System.Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
+        #endregion
+
+        #region Conexão com distinção de ambientes de execução
+        //public eCommerceContext(DbContextOptions<eCommerceContext> options) : base(options)
+        //{
+            
+        //}
+        #endregion
+
+        public DbSet<Usuario>? Usuarios { get; set; }
+        public DbSet<Contato>? Contatos { get; set; }
+        public DbSet<EnderecoEntrega>? EnderecosEntrega { get; set; }
+        public DbSet<Departamento>? Departamentos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>().Navigation(a => a.Contato).AutoInclude();
+        }
+    }
+}
